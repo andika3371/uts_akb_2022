3439e848027fae278b227aca7787ecee566fe4da
andika3371 <abdmanjiah@gmail.com>
2022-06-07 23:45:22 +0700

    first commit

---
+++ app/src/main/java/com/example/uts_andika_10119101/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.example.uts_andika_10119101;
+
+//nim : 10119101
+//nama : Andika Putra
+//kelas : if-3
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
